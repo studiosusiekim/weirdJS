4b8950b344f2afa8c706f7d1b93a005ca54e89ef
Susie Kim <hello@susiekim.de>
2020-01-12 14:53:46 +0100

    51. Functional Programming - Part 2

---
+++ 52weird.js
@@ -0,0 +1,14 @@
+// underscoreJS Library
+// underscore.js implements a lot of functional programming concepts.
+// https://underscorejs.org/docs/underscore.html
+
+//
+
+// 1. Download underscore.js
+// 2. Reference it above the .js file in the html file
+
+// The immediately invoked function expression that wraps all that     
+// Underscore code will execute, and then it wil be available as an object in the global    object to use.
+
+// The name of the underscore object is _ (only underscore)
+// Only _ is required to use the method
